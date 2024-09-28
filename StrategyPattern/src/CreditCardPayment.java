44b225eac8a2136a43706383a58789d9f9b56a1f
Harsh <sayharshfor11@gmail.com>
2024-09-28 17:42:16 +0530

    message changed for credit card payment

--- StrategyPattern/src/CreditCardPayment.java
+++ StrategyPattern/src/CreditCardPayment.java
@@ -2,7 +2,7 @@ public class CreditCardPayment implements PaymentStrategy{
 
     @Override
     public void pay(int amount){
-        System.out.println("Amount paid by credit card" + amount);
+        System.out.println("Amount paid by credit card " + amount);
     }
 
 }
